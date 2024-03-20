54799a2a09e3793e0ccc0cf05ce513dd554a5b36
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-03-21 02:29:11 +0300

    just a test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Joy';
+const welcomeMessage = 'Welcome to Flatbook, ' + 'Joy'+'!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
